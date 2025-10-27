82c642f26ad354debf25937203c87a7f39efcfc7
cl-journeyor <genericobject711@yahoo.com>
2025-10-26 19:51:14 -0700

    Project is ready

---
+++ test/jnorlib_problems/core_test.clj
@@ -0,0 +1,7 @@
+(ns jnorlib-problems.core-test
+  (:require [clojure.test :refer :all]
+            [jnorlib-problems.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
